c0f50ab5782306accd6a2cc1645db02094c483c5
Ivan Čurić <2827047+ivancuric@users.noreply.github.com>
2019-11-10 00:13:37 +0100

    added raf and network batching

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { html } from 'lighterhtml';
-import onChange from 'on-change';
 import './styles.scss';
 
 const API_URL = 'https://hacker-news.firebaseio.com/v0';
+
+export function rafPromise() {
+  return new Promise(requestAnimationFrame);
+}
 
 interface story {
   by: string;
@@ -18,40 +21,77 @@ interface story {
 const DEFAULT_HEIGHT = 40;
 const POST = 10;
 const PRE = 3;
-let currentIndex = 0;
 
 (async () => {
+  let nextIndexToRender = 0;
+  const processedIndexArray: number[] = [];
+  let renderQueue: number[] = [];
   const list = document.querySelectorAll('.list')[0];
   const window = document.querySelectorAll('.window')[0];
   const h = window.getBoundingClientRect().height;
   const n = Math.round(h / DEFAULT_HEIGHT);
 
-  // console.log(n);
-  const stories: number[] = await (await fetch(
-    `${API_URL}/newstories.json`,
-  )).json();
+  const fetchStories = async () => {
+    const response = await fetch(`${API_URL}/newstories.json`);
+    const parsed = await response.json();
+    return parsed;
+  };
 
-  const subset = stories.slice(currentIndex, n + POST);
+  const fetchItem = async (item: number, i: number) => {
+    const response = await fetch(`${API_URL}/item/${item}.json`);
+    const parsed = await response.json();
+    batchItems(i);
+    return parsed;
+  };
+
+  const batchItems = async (i: number) => {
+    processedIndexArray.push(i);
+
+    if (i > nextIndexToRender) {
+      return;
+    }
+
+    const currentBatch: number[] = [];
+
+    for (let j = nextIndexToRender; j < workingSubset.length; j++) {
+      if (processedIndexArray.includes(j)) {
+        currentBatch.push(j);
+      } else {
+        nextIndexToRender = j;
+        break;
+      }
+    }
+
+    // RENDER STUFF
+    pushToRenderQueue(currentBatch);
+  };
+
+  const pushToRenderQueue = async (currentBatch: number[]) => {
+    renderQueue.push(...currentBatch);
+    const raf = await rafPromise();
+    if (!renderQueue.length) {
+      return;
+    }
+    console.log(renderQueue, raf);
+    renderQueue = [];
+  };
+
+  // console.log(n);
+  const stories: number[] = await fetchStories();
+
+  const workingSubset = stories.slice(0, 20);
 
   const promiseArray = await Promise.all(
-    subset.map((item: number) => fetch(`${API_URL}/item/${item}.json`)),
+    workingSubset.map((id: number, i) => fetchItem(id, i)),
   );
 
-  console.log(promiseArray);
+  // const items: story[] = await Promise.all(promiseArray.filter(Boolean));
 
-  const watchedArray = onChange(promiseArray, (path, value) => {
-    console.log(path, value);
-  });
-
-  const items: story[] = await Promise.all(
-    promiseArray.map(x => x.json()),
-  ).then(x => x.filter(Boolean));
-
-  items.forEach(item =>
-    list.appendChild(
-      html`
-        <div class="list-item">${item.title}</div>
-      `,
-    ),
-  );
+  // items.forEach(item =>
+  //   list.appendChild(
+  //     html`
+  //       <div class="list-item">${item.title}</div>
+  //     `,
+  //   ),
+  // );
 })();
