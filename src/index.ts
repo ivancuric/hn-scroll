d7146298c2ae776015a70bedf3f609edbb9741d7
Ivan Čurić <2827047+ivancuric@users.noreply.github.com>
2019-11-09 22:14:19 +0100

    wip algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { html } from 'lighterhtml';
+import onChange from 'on-change';
+import './styles.scss';
 
 const API_URL = 'https://hacker-news.firebaseio.com/v0';
 
@@ -13,25 +15,42 @@ interface story {
   url: string;
 }
 
+const DEFAULT_HEIGHT = 40;
+const POST = 10;
+const PRE = 3;
+let currentIndex = 0;
+
 (async () => {
+  const list = document.querySelectorAll('.list')[0];
+  const window = document.querySelectorAll('.window')[0];
+  const h = window.getBoundingClientRect().height;
+  const n = Math.round(h / DEFAULT_HEIGHT);
+
+  // console.log(n);
   const stories: number[] = await (await fetch(
     `${API_URL}/newstories.json`,
   )).json();
 
-  const subset = stories.slice(0, 50);
+  const subset = stories.slice(currentIndex, n + POST);
 
   const promiseArray = await Promise.all(
     subset.map((item: number) => fetch(`${API_URL}/item/${item}.json`)),
   );
+
+  console.log(promiseArray);
+
+  const watchedArray = onChange(promiseArray, (path, value) => {
+    console.log(path, value);
+  });
 
   const items: story[] = await Promise.all(
     promiseArray.map(x => x.json()),
   ).then(x => x.filter(Boolean));
 
   items.forEach(item =>
-    document.body.appendChild(
+    list.appendChild(
       html`
-        <div>${item.title}</div>
+        <div class="list-item">${item.title}</div>
       `,
     ),
   );
