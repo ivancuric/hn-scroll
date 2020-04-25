e555bc3348e90edef044abfda5dc0e5f0409bded
Ivan Čurić <ivan.curic@gmail.com>
2020-04-25 10:32:01 +0200

    add urls, update packages

--- src/listItem.ts
+++ src/listItem.ts
@@ -5,6 +5,6 @@ export const listItem = (index: number, item: Story): DocumentFragment =>
   document.createRange().createContextualFragment(
     `<li class="list-item">
       <div class="count">${index + 1}.</div>
-      ${item?.title}
+      <a href=${item.url} target="_blank" rel="noopener">${item?.title}</a>
     </li>`,
   );
