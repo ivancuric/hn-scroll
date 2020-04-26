229f111caf27ccccca518984eaf385aac3c13519
Ivan Čurić <ivan.curic@gmail.com>
2020-04-26 12:33:58 +0200

    not all items have urls

--- src/listItem.ts
+++ src/listItem.ts
@@ -5,6 +5,10 @@ export const listItem = (index: number, item: Story): DocumentFragment =>
   document.createRange().createContextualFragment(
     `<li class="list-item">
       <div class="count">${index + 1}.</div>
-      <a href=${item.url} target="_blank" rel="noopener">${item?.title}</a>
+      ${
+        item.url
+          ? `<a href=${item.url} target="_blank" rel="noopener">${item?.title}</a>`
+          : `<div>${item?.title}</div>`
+      }
     </li>`,
   );
